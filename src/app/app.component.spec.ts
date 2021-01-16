4f607ea443a2b5154e5295b50c74d27c08091215
stackpond <stackpond@gmail.com>
2021-01-17 10:59:19 +1100

    Adding tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'ethereum-explorer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ethereum-explorer');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ethereum-explorer app is running!');
+    expect(app.title).toEqual('Ethereum Explorer');
   });
 });
--- src/app/components/transactions/transactions.component.spec.ts
+++ src/app/components/transactions/transactions.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TransactionsComponent } from './transactions.component';
 
 describe('TransactionsComponent', () => {
@@ -8,9 +9,9 @@ describe('TransactionsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TransactionsComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [TransactionsComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
