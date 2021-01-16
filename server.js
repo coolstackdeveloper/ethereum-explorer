a5edecd4018bcb7c2b5341ba90924fa02d4d6e18
stackpond <stackpond@gmail.com>
2021-01-16 18:42:32 +1100

    Fixed server

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 var express = require('express');
-var app = express();
-const serveStatic = require('serve-static');
-const cors = require('cors');
-
-app.use(cors());
-app.use(express.json());
-app.use(serveStatic(__dirname + "/"));
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-const port = process.env.PORT || 5000;
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log('Server started ' + port);
+console.log('server started ' + port);
